cab0bb586f2382fdf386984a1a1acbfec4e71843
ryan harper <ryan.harper.rh@gmail.com>
2020-01-03 14:44:34 -0600

    Added the dice png files 1-6.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() => runApp(DiceApp());
@@ -9,7 +10,13 @@ class DiceApp extends StatefulWidget {
 
 class _DiceAppState extends State<DiceApp> {
   @override
+  int leftDieNumber = 1;
+  int rightDieNumber = 1;
   Widget build(BuildContext context) {
+    void changeDice(){
+      leftDieNumber = Random().nextInt(6)+1;
+      rightDieNumber = Random().nextInt(6)+1;
+    }
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -20,7 +27,24 @@ class _DiceAppState extends State<DiceApp> {
         ),
         backgroundColor: Colors.red,
         body: Center(
-
+          child: Row(
+            children: <Widget>[
+              Expanded(child: 
+              FlatButton(onPressed:
+              changeDice,
+                  child: Image.asset(
+                      'images/inverted-dice-$leftDieNumber.png'),
+              ),
+              ),
+              Expanded(child: 
+              FlatButton(onPressed:
+              changeDice,
+                  child: Image.asset(
+                      'images/inverted-dice-$rightDieNumber.png'),
+              ),
+              ),
+            ],
+          ),
         ),
       ),
     );
