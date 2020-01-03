d5a02d12c21e98b3f4e93df4b32081e0402fcc3d
ryan harper <ryan.harper.rh@gmail.com>
2020-01-03 07:53:46 -0600

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(DiceApp());
+
+class DiceApp extends StatefulWidget {
+  @override
+  _DiceAppState createState() => _DiceAppState();
+}
+
+class _DiceAppState extends State<DiceApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
